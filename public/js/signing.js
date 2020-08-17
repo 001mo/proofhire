3ada151b21a21abe72f66e27daf8d750eed18e86
your_username <ahmedmohamadkhalaf@gmail.com>
2020-08-17 09:22:50 +0200

    added a function for input focus effects

--- public/js/signing.js
+++ public/js/signing.js
@@ -1,56 +1,63 @@
 $(document).ready(function (){
 
-    // Input effects area
-    var input_control = $('input.form-control');
+    // ------------------------- INPUTS EFFECTS AREA -------------------------
+    const input_control = $('.form-control , button#eyes').not('button[type=submit] , [type=submit]');
+    const input_label = 'span.input-label';
+    const input_label_focused = 'input-label-focused';
+    const input_group_focused = 'input-group-focused';
+    
 
-    input_control.on('focus' , function (){
-        // input group have the border
-        $(this).parent().css({
-            'border-color' : '#ff5500' 
-            //,'padding-top' : '16px'
-        });
-            
-        // input label span
-        $(this).parent().children('span.input-label').css({
-            'bottom' : '36px' ,
-            'left' : '5px' ,
-            'color' : '#ff5500' ,
-            'font-size' : '12px'
-        });
+    function input_event (input, event_happend){
+        switch (event_happend) {
+            case 'focus':
+                input.parent().addClass(input_group_focused);
+                input.parent().children(input_label).addClass(input_label_focused);
+                break;
+
+            case 'blur':
+                input.parent().removeClass(input_group_focused);
+                input.parent().children(input_label).removeClass(input_label_focused);
+                break;
+
+            case 'hover':
+                input.parent().addClass(input_group_focused);
+                break;
+
+            default:
+                console.log('input_event() unexpected this parameter value ' +'"'+event_happend+'"');
+                break;
+        }
+    }
+
+    input_control.focus(function (){
+        input_event($(this), 'focus');
     });
 
 
     input_control.blur(function (){
-        if( $(this).val() === '' ){
-            // input group have the border
-            $(this).parent().css({
-                'border-color' : '#aaaaaa' ,
-                'padding-top' : '5px'
-            });
+        if($(this).not('button#eyes').val() === ''){
+            input_event($(this), 'blur');
+        }
 
-            // input label span
-            $(this).parent().children('span.input-label').css({
-                'bottom' : '5px' ,
-                'left' : '39px' ,
-                'color' : '#505050' ,
-                'font-size' : '17px'
-            });
+        else if($('button#eyes').parent().children('input#password').val() === ''){
+            input_event($(this), 'blur');
         }
 
         else {
-            return false ;
+            return false;
         }
     });
 
-
+    // AUTO FOCUS ON FIRST INPUT
     $('form :input:enabled:visible:first').focus();
 
 
-    // button of password visibility
-    var password = $("#password");
-    var eyes = $("#eyes");
-    var eye = $("#eye");
-    var eye_slash = $("#eye-slash");
+
+    // -------------------------- BUTTON OF PASSWORD VISIBILITY --------------------
+    const password = $("#password"); // PSSWORD INPUT
+    const eyes = $("#eyes"); // BUTTON CONTAIN THE BOTH OF EYES
+    const eye = $("#eye"); // EYE SVG 'HIDDEN'
+    const eye_slash = $("#eye-slash"); // EYE SLASH SVG
 
     eyes.click(function () {
         if(password.attr("type") == "password"){
@@ -58,7 +65,7 @@ $(document).ready(function (){
             eye_slash.addClass("display-none");
             eye.show();
         }
-        
+
         else {
             password.attr("type" , "password");
             eye.hide();
